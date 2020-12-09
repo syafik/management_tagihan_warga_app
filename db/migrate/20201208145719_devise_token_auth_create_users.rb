0dc153773323725fb974525e6b41615a35e168b5
syafik <syafikli_musyafako@yahoo.com>
2020-12-09 12:51:40 +0700

    fix migration error

--- db/migrate/20201208145719_devise_token_auth_create_users.rb
+++ db/migrate/20201208145719_devise_token_auth_create_users.rb
@@ -12,7 +12,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
     end
 
     add_index :users, [:uid, :provider],     unique: true
-    add_index :users, :reset_password_token, unique: true
-    add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
   end
 end
