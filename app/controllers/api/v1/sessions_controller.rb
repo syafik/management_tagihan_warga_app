e3d2c15468d1fc426ec8fc1bf64e990d09f8f886
syafik <syafikli_musyafako@yahoo.com>
2020-12-16 22:13:13 +0700

    update json response when login failed

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,7 +6,7 @@ module Api
       skip_before_action :verify_authenticity_token, only: :create, raise: false
 
       def render_create_success
-        render json: {
+        render status: 200, json: {
           success: true,
           message: "Login berhasil.",
           me: @resource,
@@ -14,6 +14,21 @@ module Api
           address: @resource.address
         }
       end
+
+      def render_new_error
+       render status: 401, json: {
+         success: false,
+         message: 'Login gagal.'
+       }
+      end
+
+      def render_create_error_bad_credentials
+        render status: 401, json: {
+          success: false,
+          message: 'Password atau email yang dimasukkan salah.'
+        }
+      end
+
     end
   end
 end
