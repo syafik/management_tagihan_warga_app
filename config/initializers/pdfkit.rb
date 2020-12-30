5cdce420b53cac3b486283a0f0efb34e7679ce74
syafik <syafikli_musyafako@yahoo.com>
2020-12-30 14:42:00 +0700

    fix pdfkit view

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -3,7 +3,13 @@
 PDFKit.configure do |config|
   config.wkhtmltopdf = '~/.rbenv/shims/wkhtmltopdf'
   config.default_options = {
-    page_size: 'A4',
-    print_media_type: true
+    print_media_type: true,
+    page_size: "A4",
+    encoding: "UTF-8",
+    ## Make sure the zoom option is not enabled!
+    ## zoom: '1.3',
+    disable_smart_shrinking: false,
+    footer_right: "Page [page] of [toPage]"
   }
+
 end
