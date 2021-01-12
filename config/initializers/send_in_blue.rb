fbe87b411d235aedfeb5c120f8d6e8de57a69e02
syafik <syafikli_musyafako@yahoo.com>
2021-01-12 15:49:15 +0700

    setup setting using detenv

---
+++ config/initializers/send_in_blue.rb
@@ -0,0 +1,11 @@
+SibApiV3Sdk.configure do |config|
+  # Configure API key authorization: api-key
+  config.api_key['api-key'] = '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'
+  # Uncomment the following line to set a prefix for the API key, e.g. 'Bearer' (defaults to nil)
+  #config.api_key_prefix['api-key'] = 'Bearer'
+
+  # Configure API key authorization: partner-key
+  # config.api_key['partner-key'] = 'YOUR API KEY'
+  # Uncomment the following line to set a prefix for the API key, e.g. 'Bearer' (defaults to nil)
+  #config.api_key_prefix['partner-key'] = 'Bearer'
+end
