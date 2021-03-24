eee043038707af1c2c238e7045269540866b9f05
Syafikli Musyafako <syafikli_musyafako@yahoo.com>
2021-03-24 16:57:42 +0700

    update some apis

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
+
 module Api
   module V1
     class HomeController < Api::V1::BaseController
       def home_page
-        render status: 200,  json: { 
-          success: true, 
+        render status: 200, json: {
+          success: true,
           me: current_user,
           tagihan: current_user.try(:address).try(:tagihan_now), 
-          last_payment: current_user.try(:address).try(:last_payment_contribution), 
-          blok: current_user.address ? current_user.address.block_address : "-",
+          last_payment: current_user.try(:address).try(:last_payment_contribution),
+          blok: current_user.address ? current_user.address.block_address : '-',
           cash_flow: CashFlow.info((Date.current-1.month).year),
           notifications: current_user.user_notifications.includes(:notification).order('created_at DESC').limit(3).as_json(methods: [:notification]),
           total_sisa_kas: CashInfo.first
@@ -17,7 +18,7 @@ module Api
 
       def user_lists
         users = User.includes(:address).where(kk: true)
-        render status: :ok, json: { success: true, 
+        render status: :ok, json: { success: true,
           users: users.as_json(methods: [:blok_name])
         }
       end
@@ -27,7 +28,7 @@ module Api
         total_cash_in = cash_flows.sum(&:cash_in)
         total_cash_out = cash_flows.sum(&:cash_out)
         grand_total = cash_flows.sum(&:total)
-        render json: { success: true, title: "Cash Flow Tahun #{params[:year]}", total_cash_in: total_cash_in, total_cash_out: total_cash_out, grand_total: grand_total, cash_flows: cash_flows.as_json(:methods => [:month_info]) }, status: :ok
+        render json: { success: true, title: "Tahun #{params[:year]}", total_cash_in: total_cash_in, total_cash_out: total_cash_out, grand_total: grand_total, cash_flows: cash_flows.as_json(:methods => [:month_info]) }, status: :ok
       end
 
       def cash_transactions
@@ -36,7 +37,7 @@ module Api
         credit_total = 0
         year_selected = params[:year] || Date.current.year
         month_selected = params[:month] || Date.current.month
-        selected_date = Date.parse("#{year_selected}-#{month_selected}-10") 
+        selected_date = Date.parse("#{year_selected}-#{month_selected}-10")
         cash_transactions = CashTransaction.where(transaction_date: selected_date.beginning_of_month..selected_date.end_of_month).order('transaction_date ASC')
         CashTransaction::REPORT_WARGA.each do |key, value|
           item = {}
@@ -70,7 +71,7 @@ module Api
             end
           end
         end
-        render json: { success: true, title: "Transaksi Kas Per #{UserContribution::MONTHNAMES.invert[params[:month].to_i]} #{params[:year]}", transactions: report_items, debit_total: debit_total, credit_total: credit_total, grand_total: debit_total-credit_total }, status: :ok
+        render json: { success: true, title: "#{UserContribution::MONTHNAMES.invert[params[:month].to_i]} #{params[:year]}", transactions: report_items, debit_total: debit_total, credit_total: credit_total, grand_total: debit_total-credit_total }, status: :ok
       end
 
       def contributions
@@ -89,16 +90,16 @@ module Api
 
       def pay_contribution
         address = Address.find(params[:address_id])
-        return render status:402, json:{success: true, message: 'pembayaran iuran gagal.', error: 'invalid address_id'} if address.nil?
+        return render status: 402, json:{ success: true, message: 'pembayaran iuran gagal.', error: 'invalid address_id' } if address.nil?
         uc = UserContribution.new(
-            month: Date.current.month,
-            year: Date.current.year,
-            address_id: address.id,
-            contribution: params[:contribution],
-            receiver_id: current_user.id,
-            pay_at: params[:pay_at],
-            payment_type: params[:payment_type],
-            blok: params[:blok]
+          month: Date.current.month,
+          year: Date.current.year,
+          address_id: address.id,
+          contribution: params[:contribution],
+          receiver_id: current_user.id,
+          pay_at: params[:pay_at],
+          payment_type: params[:payment_type],
+          blok: params[:blok]
         )
         if uc.save
           1.upto(params[:total_bayar].to_i-1) do |_i|
@@ -115,11 +116,14 @@ module Api
             total: (params[:total_bayar].to_i*params[:contribution].to_f),
             pic_id: current_user.id
           )
-          notification = Notification.create(title: "Pembayaran Iuran Bulanan Sukses!", notif: "Terima Kasih atas pembayara iuran bulanan yang telah dilakukan. \n Anda membayar sebanyak #{params[:total_bayar]} kali tagihan sebesar #{(params[:total_bayar].to_i*params[:contribution].to_f)}, dan sudah diterima oleh #{current_user.name} Secara #{params[:payment_type].to_i == 2 ? 'TRANSFER' : 'CASH'} tanggal #{uc.pay_at.strftime('%d %B %Y')}.")
+          notification = Notification.create(
+            title: 'Pembayaran Iuran Bulanan Sukses!',
+            notif: "Terima Kasih atas pembayara iuran bulanan yang telah dilakukan. \n Anda membayar sebanyak #{params[:total_bayar]} kali tagihan sebesar #{(params[:total_bayar].to_i*params[:contribution].to_f)}, dan sudah diterima oleh #{current_user.name} Secara #{params[:payment_type].to_i == 2 ? 'TRANSFER' : 'CASH'} tanggal #{uc.pay_at.strftime('%d %B %Y')}."
+          )
           SendNotificationToUsersJob.perform_later(notification.id, 1, address.users.pluck(:id))
-          render json: { success: true, message: 'pembayaran iuran berhasil dilakukan.'}, status: :ok
+          render json: { success: true, message: 'pembayaran iuran berhasil dilakukan.' }, status: :ok
         else
-          render status:402, json:{success: false, message: "Pembayaran iuran gagal. #{uc.errors.full_messages.join(", ")}"}
+          render status: 402, json: { success: false, message: "Pembayaran iuran gagal. #{uc.errors.full_messages.join(", ")}" }
         end
       end
 
@@ -129,21 +133,24 @@ module Api
           year: Date.current.month,
           transaction_date: params[:transaction_date],
           transaction_type: params[:transaction_type],
-          transaction_group: params[:transaction_group] ,
-          description: params[:description] ,
+          transaction_group: params[:transaction_group],
+          description: params[:description],
           total: params[:total],
           pic_id: current_user.id
         )
         if ct.save
           render json: { success: true, message: 'transaksi berhasil disimpan.' }, status: :ok
         else
-          render status:402, json:{success: false, message: 'transaksi gagal disimpan.', error: ct.errors}
+          render status: 402, json: { success: false, message: 'transaksi gagal disimpan.', error: ct.errors }
         end
       end
 
       def notifications
-        user_notifications =  current_user.user_notifications.includes(:notification).order('created_at DESC').limit(10)
-        render json: { success: true, user_notifications: user_notifications.as_json(methods: [:notification, :notif_sent_at]) }, status: :ok
+        user_notifications = current_user.user_notifications.includes(:notification).order('created_at DESC').limit(10)
+        render status: :ok, json: {
+          success: true,
+          user_notifications: user_notifications.as_json(methods: [:notification, :notif_sent_at])
+          }
       end
 
       def notification_show
@@ -156,54 +163,65 @@ module Api
         notification = Notification.new(title: params[:title], notif: params[:notif])
         if notification.save
           SendNotificationToUsersJob.perform_later(notification.id, 1, [])
-          render json: { success: true, message: 'notifikasi berhasil disimpan dan dikirim.'}, status: :ok
+          render json: { success: true, message: 'notifikasi berhasil disimpan dan dikirim.' }, status: :ok
         else
-          render status:402, json:{success: false, message: 'notifikasi gagal disimpan.', error: notification.errors}
+          render status: 402, json: {
+            success: false,
+            message: 'notifikasi gagal disimpan.',
+            error: notification.errors
+          }
         end
       end
 
       def debts
         debts = current_user.debts
-        total_pinjam = debts.select{|d| d.debt_type == 1}.sum(&:value)
-        total_bayar = debts.select{|d| d.debt_type == 2}.sum(&:value)
+        total_pinjam = debts.select { |d| d.debt_type == 1 }.sum(&:value)
+        total_bayar = debts.select { |d| d.debt_type == 2 }.sum(&:value)
         sisa_hutang = total_pinjam - total_bayar
-        render status: :ok, json: {success: true, debts: debts, total_pinjam: total_pinjam, total_bayar: total_bayar, sisa_hutang: sisa_hutang}
+        render status: :ok, json: {
+          success: true,
+          debts: debts,
+          total_pinjam: total_pinjam,
+          total_bayar: total_bayar,
+          sisa_hutang: sisa_hutang
+        }
       end
 
-      def add_debt
-          
-      end
+      def add_debt; end
 
       def installments
         installments = Installment.includes(:installment_transactions).where('parent_id IS NULL')
-        render status: :ok, json: {success: true, installments: installments.as_json(:methods => [:total_paid, :remaining_installment, :paid_off?])}
+        render status: :ok, json: {
+          success: true,
+          installments: installments.as_json(methods: [:total_paid, :remaining_installment, :paid_off?])
+        }
       end
 
       def installment_transaction
         installment = Installment.find(params[:id])
         installment_transactions = installment.installment_transactions
-        render status: :ok, json: {success: true, installment: installment.as_json(:methods => [:total_paid, :remaining_installment, :paid_off?]), installment_transactions: installment_transactions}
+        render status: :ok, json: {
+          success: true,
+          installment: installment.as_json(methods: [:total_paid, :remaining_installment, :paid_off?]),
+          installment_transactions: installment_transactions
+        }
       end
 
-      def add_installment
-        
-      end
+      def add_installment; end
 
-      def pay_installment
-
-      end
+      def pay_installment; end
 
       def create_user
         user = User.new(create_user_params)
         user.kk = false
         user.address_id = current_user.address_id
-        password = [*('a'..'z'),*('0'..'9')].shuffle[0,8].join
+        password = [*('a'..'z'), *('0'..'9')].sample(8).join
         user.password = password
         if user.save
           SendPasswordToEmailJob.perform_later(user.id, password)
-          render status: 200, json: {success: true, message: 'success, password telah dikirim ke email.'}
+          render status: 200, json: { success: true, message: 'success, password telah dikirim ke email.' }
         else
-          render status:402, json:{success: false, message: 'gagal.', error: user.errors}
+          render status: 402, json: { success: false, message: 'gagal.', error: user.errors }
         end
       end
 
@@ -212,7 +230,6 @@ module Api
       def create_user_params
         params.require(:user).permit(:email, :name, :phone_number)
       end
-
     end
   end
 end
