a64b54848856dee109298f273db665b165430282
Syafikli Musyafako <syafiklijtk04@gmail.com>
2025-09-13 20:15:45 +0700

    jobs

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationJob < ActiveJob::Base
+  # Automatically retry jobs that encounter a DeadlockRetry::Deadlock
+  # retry_on ActiveRecord::Deadlocked
+
+  # Most jobs are safe to ignore if the underlying records are no longer available
+  # discard_on ActiveJob::DeserializationError
+  
+  # Set default queue name
+  queue_as :default
+  
+  # Set default retry attempts
+  retry_on StandardError, wait: 5.seconds, attempts: 3
 end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module SwitchupAdministrator
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    # ActiveJob configuration
+    config.active_job.queue_adapter = :delayed_job
   end
 end
