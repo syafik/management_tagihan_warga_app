05e8784c9c36122989b1d40309470586524b0422
Syafikli Musyafako <syafiklijtk04@gmail.com>
2025-09-13 13:06:49 +0700

    fix

--- config/puma_production.rb
+++ config/puma_production.rb
@@ -1,24 +1,25 @@
 # config/puma_production.rb
-#!/usr/bin/env puma
+# !/usr/bin/env puma
 
-directory '/home/deploy/management_tagihan_warga_app'
-rackup '/home/deploy/management_tagihan_warga_app/config.ru'
+directory '/var/www/puriayana-app'
+rackup '/var/www/puriayana-app/config.ru'
 environment 'production'
 
-pidfile '/home/deploy/management_tagihan_warga_app/tmp/pids/puma.pid'
-state_path '/home/deploy/management_tagihan_warga_app/tmp/pids/puma.state'
-stdout_redirect '/home/deploy/management_tagihan_warga_app/log/puma.log', '/home/deploy/management_tagihan_warga_app/log/puma_err.log', true
+pidfile '/var/www/puriayana-app/tmp/pids/puma.pid'
+state_path '/var/www/puriayana-app/tmp/pids/puma.state'
+stdout_redirect '/var/www/puriayana-app/log/puma.log',
+                '/var/www/puriayana-app/log/puma_err.log', true
 
 threads 2, 16
 workers 2
 
-bind 'unix:///home/deploy/management_tagihan_warga_app/tmp/sockets/puma.sock'
+bind 'unix:///var/www/puriayana-app/tmp/sockets/puma.sock'
 
 preload_app!
 
 on_restart do
   puts 'Refreshing Gemfile'
-  ENV["BUNDLE_GEMFILE"] = '/home/deploy/management_tagihan_warga_app/Gemfile'
+  ENV['BUNDLE_GEMFILE'] = '/var/www/puriayana-app/Gemfile'
 end
 
 on_worker_boot do
