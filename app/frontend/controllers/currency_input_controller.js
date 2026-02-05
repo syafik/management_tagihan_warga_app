2ec0a4ecec44df0a46908472d5285b7dd74ea513
Syafikli Musyafako <syafiklijtk04@gmail.com>
2026-02-05 08:56:45 +0700

    add rates view

--- app/frontend/controllers/currency_input_controller.js
+++ app/frontend/controllers/currency_input_controller.js
@@ -2,17 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["display", "hidden"]
+  static values = {
+    prefix: String
+  }
 
   connect() {
     // Initialize with current value if any
     if (this.hiddenTarget.value) {
-      this.displayTarget.value = this.formatCurrency(this.hiddenTarget.value)
+      const rawValue = this.normalizeValue(this.hiddenTarget.value)
+      this.hiddenTarget.value = rawValue
+      this.displayTarget.value = this.formatCurrency(rawValue)
     }
   }
 
   format(event) {
     // Get raw value (remove all non-digits)
-    const rawValue = this.displayTarget.value.replace(/\D/g, '')
+    const rawValue = this.normalizeValue(this.displayTarget.value)
 
     // Update hidden field with raw number
     this.hiddenTarget.value = rawValue
@@ -25,6 +30,11 @@ export default class extends Controller {
     if (!value) return ''
 
     // Convert to string and add thousand separators
-    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+    const formattedValue = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+    return this.prefixValue ? `${this.prefixValue} ${formattedValue}` : formattedValue
+  }
+
+  normalizeValue(value) {
+    return value.toString().replace(/\D/g, '')
   }
 }
